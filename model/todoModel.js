0bc65e61b07fc526f624b4a1cd15e7722542ef4e
BEATRIZ CORRÊA <66252245+bdscorrea@users.noreply.github.com>
2025-10-03 20:26:54 -0300

    testes ok

--- model/todoModel.js
+++ model/todoModel.js
@@ -5,6 +5,20 @@ const todos = [
   "title": "TESTANDO APLICAÇÃO",
   "status": "A",
   "description": "testando aplicação"
+},
+{
+  "id": 2,
+  "userId": 2,
+  "title": "TESTANDO APLICAÇÃO 2",
+  "status": "D",
+  "description": "testando aplicação"
+},
+{
+  "id": 3,
+  "userId": 1,
+  "title": "TESTANDO APLICAÇÃO 3",
+  "status": "A",
+  "description": "testando aplicação"
 }
 ]; 
 
